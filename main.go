ed5597fb3f303fb42ec42f2e8755e64f3fa8b54e
xSnaKx13 <152309157+xSnaKx13@users.noreply.github.com>
2025-07-28 10:39:54 +0000

    new func

--- main.go
+++ main.go
@@ -9,5 +9,13 @@ func main()  {
 	const usdToRub = 90.0
 	const eurToRub = eurToUsd * usdToRub
 
-	fmt.Println(eurToRub)
+}
+func inputUserValue(usingValute, convertValue float64){
+	fmt.Println("Введите валюту которая у вас есть")
+	fmt.Scan(&usingValute)
+	fmt.Println("Введите валюту на которую необходимо обменять")
+	fmt.Scan(&convertValue)
+}
+func convert (usingValute, convertValue, howMatch float64) float64{
+	
 }
