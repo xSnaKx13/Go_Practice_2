c401544934553852c3033b6d8275f90363903fa4
xSnaKx13 <152309157+xSnaKx13@users.noreply.github.com>
2025-07-30 11:59:32 +0000

    test

--- main.go
+++ main.go
@@ -7,28 +7,28 @@ import (
 
 func main(){
 	var transactions[] float64 
-
+    var err error
 	for{
 		fmt.Print("Введите транзакцию: ")
-		transactionN, err := newTransaction()
+		transactionN, err = newTransaction()
 		if err!=nil{
-			fmt.Println(err)
+			fmt.Println("Ошибка: ", err)
+			continue
 		}
 		if transactionN == 0{
 			break
 		}
 		transactions = append(transactions, transactionN)
-	}
+
 	fmt.Print(transactions)
 }
 
-func newTransaction()(float64, error){
+func newTransaction() (float64 , error){
 	var transaction float64 
 	var err error
 	_, err = fmt.Scan(&transaction)
 	if err!=nil{
-		errors.New("Не является транзакцией")
-		return 0, err
+		return 0, errors.New("Не является транзакцией")
 	}
 	return transaction, nil
 }
